963e117e042d4b71fd296f24ac60f01375346553
HydraulicSheep <24516366+HydraulicSheep@users.noreply.github.com>
2020-12-16 02:45:44 +1100

    Added new collision method

--- main.js
+++ main.js
@@ -96,34 +96,43 @@ function update_canvas() {
     for (item of items) {
         bounding_boxes.push(item.getBoundingBox())
     }
-    var i=0;
-    for (i=0; i<items.length;i++) {
-        for (j=i+1;j<items.length;j++) {
+
+    for (var i=0; i<items.length;i++) {
+        for (var j=i+1;j<items.length;j++) {
+            console.log(i)
             if (bounding_intersection_check(bounding_boxes[i], bounding_boxes[j])) {
-                console.log('COLLISION!!!')
+                
                 v1 = items[i].v
-                v1_t = get_vec(bounding_boxes[i],bounding_boxes[j])
                 v2 = items[j].v
-                v2_t = vec_mul(v1_t,-1)
-                console.log(v1_t)
-                console.log(v2_t)
-                v1_parallel = vec_mul(v1_t,dotp(v1,v1_t)/(mag(v1_t)**2))
-                v1_perp = vec_sub(v1,v1_parallel)
-                res = vec_add(v1_perp, vec_mul(v1_parallel,-1))
-                t = vec_sub(res,v1)
-                console.log(dotp(v1,v1_t))
-                console.log(v1_perp)
-                action = [items[i],0,0,t[0],t[1]];
-                collision_queue.push(action)
 
-                v2_parallel = vec_mul(v2_t,dotp(v2,v2_t)/(mag(v2_t)**2))
-                v2_perp = vec_sub(v2,v2_parallel)
-                res = vec_add(v2_perp, vec_mul(v2_parallel,-1))
-                t = vec_sub(res,v2)
+                // Vector between bounding box centres
+                vCollision = get_vec(bounding_boxes[i],bounding_boxes[j]) 
 
-                action = [items[j],0,0,t[0],t[1]];
+                // Unit Vector in direction between bounding box centres
+                collisionDir = vec_mul(vCollision,1/(mag(vCollision))) 
+
+                // Relative velocity of v1 to v2.
+                // If vrel > 0, object 1 is moving to the right relative to object 2
+                // If vrel < 0, object 1 is moving to the left relative to it object 2.
+                // Can think of it as locking object 2 and watching object 1 move.
+                vRel = [v1[0]-v2[0],v1[1]-v2[1]]
+                
+                // Resolves relative velocity parallel to collision vector.
+                // Gives parallel relative speed.
+                para_speed_rel = dotp(vRel,collisionDir)
+                
+                /* Relative motion opposite to collision vector. => Already moving apart so no more colliding needs
+                    to be done. */
+                if (para_speed_rel<0) {
+                    continue;
+                }
+
+                action = [items[i],0,0,-para_speed_rel*collisionDir[0],-para_speed_rel*collisionDir[1]];
                 collision_queue.push(action)
                 console.log(action)
+
+                action = [items[j],0,0,para_speed_rel*collisionDir[0],para_speed_rel*collisionDir[1]];
+                collision_queue.push(action)
 
             }
         }
